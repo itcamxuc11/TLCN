9ff85550da8bea0a942f4dc0a32180a55b164bbf
unknown <duytrankc1@gmail.com>
2019-12-30 22:47:52 +0700

    ok

---
+++ app/src/main/java/com/example/myapplication/StartActivity.java
@@ -0,0 +1,43 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.myapplication.Fragment.FragmentSearchActivity;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+    public void onTopic(View view){
+        Intent intent = new Intent(StartActivity.this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void onSearch2(View view){
+        FragmentManager fragmentManager= getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction= fragmentManager.beginTransaction();
+        FragmentSearchActivity fragmentSearchActivity= new FragmentSearchActivity();
+        fragmentTransaction.replace(R.id.framecontent,fragmentSearchActivity);
+        fragmentTransaction.commit();
+    }
+
+    public void onHomeWork(View view){
+        Intent intent = new Intent(StartActivity.this, HomeWorkActivity.class);
+        startActivity(intent);
+    }
+    public void onDailyWord(View view){
+        Intent intent = new Intent(StartActivity.this, DaillyWordActivity.class);
+        startActivity(intent);
+    }
+}
